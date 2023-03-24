b986f0909d70bfbd3472aae6acdbdc9ffc6d8aef
peyoon <127940087+peyoon@users.noreply.github.com>
2023-03-24 14:20:14 +0900

    Add files via upload

---
+++ webhw1/web.js
@@ -0,0 +1,6 @@
+function calculate() {
+  const num1 = Number(document.getElementById('num1').value);
+  const num2 = Number(document.getElementById('num2').value);
+  const result = num1 + num2;
+  document.getElementById('result').textContent = result;
+}
